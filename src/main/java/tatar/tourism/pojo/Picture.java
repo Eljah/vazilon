275609751a947e195f93625e6f5ba0f4e2ddcfa0
Ilya <eljah@mail.ru>
2015-11-22 18:24:27 +0300

    javadoc added to the Picture

--- src/main/java/tatar/tourism/pojo/Picture.java
+++ src/main/java/tatar/tourism/pojo/Picture.java
@@ -2,15 +2,45 @@ package tatar.tourism.pojo;
 
 /**
  * Created by Ilya Evlampiev on 10.11.2015.
+ * @author Ilya Evlampiev
+ */
+
+/**
+ * POJO Model of Picture. Defines the photo coordinates and shot direction in degrees, description, owner user, original filename and upload time.
+ * Модель изображения. Определяет координаты фотографии, направление съемки, описания, хозяина изображения, изначальное название файла и время загрузки.
  */
 public class Picture {
     int id;
+    /**
+     * Original filename
+     */
     String originName;
+    /**
+     * Upload Date
+     */
     String uploadDate;
+    /**
+     * User that owns this picture
+     */
     User uploader;
+    /**
+     * Textual description
+     */
     String description;
+    /**
+     * Geographical latitude
+     * Географическая широта
+     */
     Float coordinateLat;
+    /**
+     * Geographical longitude
+     * Географическая долгота
+     */
     Float coordinateLong;
+    /**
+     * Shot degree in degrees, counted from the North clockwise
+     * Угол съемки, отсчитываемый от направления на север по часовой стрелке
+     */
     Double degree;
 
     public String getOriginName() {
@@ -49,6 +79,12 @@ public class Picture {
         return coordinateLat;
     }
 
+
+    /**
+     * Latitude is set there
+     * Устанавливается значение широты
+     * @param coordinateLat parameter use Fload values or can be null
+     */
     public void setCoordinateLat(Float coordinateLat) {
         this.coordinateLat = coordinateLat;
     }
@@ -57,6 +93,12 @@ public class Picture {
         return coordinateLong;
     }
 
+
+    /**
+     * Longitude is set there
+     * Устанавливается значение долготы
+     * @param coordinateLong parameter use Fload values or can be null
+     */
     public void setCoordinateLong(Float coordinateLong) {
         this.coordinateLong = coordinateLong;
     }
@@ -65,10 +107,26 @@ public class Picture {
         return degree;
     }
 
+    /**
+     * Shot degree is set there
+     * Устанавливается угла съемки
+     * @param degree parameter use Double values or can be null
+     */
     public void setDegree(Double degree) {
         this.degree = degree;
     }
 
+
+    /**
+     * Coordinates and degeree are set up here from 2 poins on the map. The first point is the photo location, the second point is used to set up the direction of the shot
+     * Устанавливаются координаты снимка и направление через координаты 2х точек на карте. Первая точка - собственно координаты снимка. Вторая точка нужна для определения направления от первой точки.
+     * @param coordinate1Lat first point (photo location) Latitude parameter use Float values
+     * @param coordinate1Long first point (photo location) Longitude parameter use Float values
+     * @param coordinate2Lat second point (photo direction) Latitude parameter use Float values
+     * @param coordinate2Long second point (photo direction) Longitude parameter use Float values
+     *
+     * Координаты в градусах отсчитываются: широта - с юга на север (т.е. экватор - ноль градусов, Северный полюс - 90), долгота - от Гринчинского меридиана на восток (т.е. Казань восточнее Москвы, т.е. долгота у нее будет больше).
+     */
     public void setCoordinatesAndDegree(Float coordinate1Lat, Float coordinate1Long, Float coordinate2Lat, Float coordinate2Long) {
         this.coordinateLat = coordinate1Lat;
         this.coordinateLong = coordinate1Long;
